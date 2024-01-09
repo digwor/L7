c862cc3871f2ebe198d523fa28f542addf3c8a1a
Bolat Akhmadiyev <bolat@akhmadiyev.ru>
2024-01-09 18:44:46 +0600

    Lab7 v1

---
+++ products/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+    category_id = models.IntegerField()
+    is_active = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
